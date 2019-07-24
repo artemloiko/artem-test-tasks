95cd01de1fabbb0f642489d2662c47dc0c26be93
abz_artem_loiko <artem.loiko@abz.agency>
2019-07-24 21:18:55 +0300

    Add test form with hooks

--- hw-task1/src/components/Form/Form.tsx
+++ hw-task1/src/components/Form/Form.tsx
@@ -1,89 +1,41 @@
-import React, { Component } from "react";
-
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import clsx from "clsx";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
-interface FormProps {}
+export function Form() {
+  const [value, setValue] = useState("");
 
-interface FormState {
-  [key: string]: string;
-}
-// firstName: string;
-// email: string;
-// emailRFC: string;
-// employeeId: string;
-// phone: string;
-// phones: string;
-// PINcode: string;
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      display: "flex",
-      flexWrap: "wrap"
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1)
-    },
-    dense: {
-      marginTop: theme.spacing(2)
-    },
-    menu: {
-      width: 200
-    }
-  })
-);
-
-export class MainForm extends Component<FormProps, FormState> {
-  constructor(props: FormProps) {
-    super(props);
-    this.state = {
-      firstName: "",
-      email: "",
-      emailRFC: "",
-      employeeId: "",
-      phone: "",
-      phones: "",
-      PINcode: ""
-    };
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
   }
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  render() {
-    const fields: string[] = [
-      "firstName",
-      "email",
-      "emailRFC",
-      "employeeId",
-      "phone",
-      "phones",
-      "PINcode"
-    ];
-
-    return (
-      <div>
-        <h1>Test form</h1>
-        <form noValidate autoComplete="off">
-          {fields.map(field => (
-            <div className="row">
-              <TextField
-                label={field}
-                value={this.state.field}
-                onChange={this.handleChange}
-                name={field}
-                margin="normal"
-                variant="outlined"
-              />
-            </div>
-          ))}
-        </form>
-      </div>
-    );
-  }
+  const fields: string[] = [
+    "firstName",
+    "email",
+    "emailRFC",
+    "employeeId",
+    "phone",
+    "phones",
+    "PINcode"
+  ];
+  return (
+    <div>
+      <h1>Test form</h1>
+      <form noValidate autoComplete="off">
+        {fields.map(field => (
+          <div className="row">
+            <TextField
+              label={field}
+              value={value}
+              onChange={handleInputChange}
+              name={field}
+              margin="normal"
+              variant="outlined"
+            />
+          </div>
+        ))}
+      </form>
+    </div>
+  );
 }
