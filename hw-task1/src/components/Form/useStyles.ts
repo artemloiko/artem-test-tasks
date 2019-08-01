8e121b3def55b49bfb7e72065ba7d16d79b948b1
abz_artem_loiko <artem.loiko@abz.agency>
2019-08-01 20:39:39 +0300

    Upd styles

--- hw-task1/src/components/Form/useStyles.ts
+++ hw-task1/src/components/Form/useStyles.ts
@@ -1,27 +1,22 @@
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      display: "flex",
-      flexWrap: "nowrap",
-      flexDirection: "column",
-      alignItems: "center"
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 300
+      display: 'flex',
+      flexWrap: 'nowrap',
+      flexDirection: 'column',
+      alignItems: 'center'
     },
     button: {
       // margin: theme.spacing(3),
-      "&.primaryButton": {
-        margin: "30px 0"
+      '&.primaryButton': {
+        margin: '30px 0'
       },
       width: 300
     },
     formControl: {
-      margin: theme.spacing(1)
+      margin: 0
     },
     formHelper: {
       maxWidth: `calc(300px - 24px)`
--- hw-task1/src/components/SimpleField/useStyles.ts
+++ hw-task1/src/components/SimpleField/useStyles.ts
@@ -1,22 +1,21 @@
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      display: "flex",
-      flexWrap: "nowrap",
-      flexDirection: "column",
-      alignItems: "center"
+      display: 'flex',
+      flexWrap: 'nowrap',
+      flexDirection: 'column',
+      alignItems: 'center'
     },
     textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
+      margin: theme.spacing(0),
       width: 300
     },
     button: {
       // margin: theme.spacing(3),
-      "&.primaryButton": {
-        margin: "30px 0"
+      '&.primaryButton': {
+        margin: '30px 0'
       },
       width: 300
     },
