8b25885b96311aaf7c8c0b72f1c6c32f60350ec3
abz_artem_loiko <artem.loiko@abz.agency>
2019-08-08 22:13:19 +0300

    Fixed some styles

--- hw-task3/src/components/PhotoUpload/PhotoUpload.js
+++ hw-task3/src/components/PhotoUpload/PhotoUpload.js
@@ -6,9 +6,12 @@ export default function PhotoUpload() {
   return (
     <div className="PhotoUpload">
       <img className="PhotoUpload__image" src={uploadImage} alt="Select it on your computer" />
-      <p className="PhotoUpload__text">Drag your photo here</p>
-      <p className="PhotoUpload__text">or</p>
-      <button className="PhotoUpload__button">Select a photo from your computer</button>
+      <p className="PhotoUpload__text">
+        Drag your photo here
+        <br />
+        or
+      </p>
+      <input type="file" className="PhotoUpload__button" />
     </div>
   );
 }
