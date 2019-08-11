16a9ffd78226533a4970a11ff1a3da58c1be9a92
abz_artem_loiko <artem.loiko@abz.agency>
2019-08-11 21:29:43 +0300

    Add file uploading

--- hw-task3/src/components/PhotoUpload/PhotoUpload.js
+++ hw-task3/src/components/PhotoUpload/PhotoUpload.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
 import uploadImage from './PhotoUpload.png';
 import './PhotoUpload.css';
 
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+    width: 320
+  },
+  input: {
+    display: 'none'
+  }
+}));
+
 export default function PhotoUpload() {
+  let [imageFile, setImageFile] = useState();
+  let fileRef = useRef();
+  const classes = useStyles();
+
+  const onFileInputChange = event => {
+    console.log('event on file input', event);
+    console.log('ref is', fileRef.current.files);
+    setImageFile(fileRef.current.files[0]);
+  };
+
   return (
     <div className="PhotoUpload">
       <img className="PhotoUpload__image" src={uploadImage} alt="Select it on your computer" />
@@ -11,7 +34,30 @@ export default function PhotoUpload() {
         <br />
         or
       </p>
-      <input type="file" className="PhotoUpload__button" />
+      <label htmlFor="fileUpload">
+        <Button variant="contained" color="primary" component="span" className={classes.button}>
+          Upload
+        </Button>
+      </label>
+      <input
+        ref={fileRef}
+        className={classes.input}
+        accept="image/*"
+        id="fileUpload"
+        type="file"
+        onChange={onFileInputChange}
+      />
+      {imageFile && (
+        <>
+          <p>Image info: </p>
+          <ol>
+            <li>Name: {imageFile.name}</li>
+            <li>Size: {imageFile.size}</li>
+            <li>Type: {imageFile.type}</li>
+          </ol>
+          <img src={URL.createObjectURL(imageFile)} alt={imageFile.name} className="PhotoUpload__preview" />
+        </>
+      )}
     </div>
   );
 }
